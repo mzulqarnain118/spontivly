081ed55eeddb84ed85562930c832af9c3bd90990
mzulqarnain118 <muhammad.zulqarnain@devcrew.io>
2023-11-16 15:32:54 +0500

    fixed bug in socialSlice

--- src/redux/socialSlice.js
+++ src/redux/socialSlice.js
@@ -19,13 +19,7 @@ const socialSlice = createSlice({
   reducers: {
     setSoicalData: (state, action) => {
       const { provider, id } = action.payload;
-
-      console.log("🚀 ~ file: socialSlice.js:35 ~ provider, id:", provider, id)
-
       state[provider].id = id;
-
-      console.log("🚀 ~ file: socialSlice.js:36 ~ state:", state)
-
     },
   },
 });
