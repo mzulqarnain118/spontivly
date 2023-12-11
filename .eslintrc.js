75fd3b9060b361b419a2b46efe967f4cbb6df94a
Saad Qureshi <saad.qureshi@devcrew.io>
2023-12-11 18:15:31 +0500

    initial-setup: eslint rules and tsconfig along with fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,87 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest": true
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:jest/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
+    'plugin:testing-library/react',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['tsconfig.json']
+  },
+  plugins: ['react', 'import', 'prettier', '@typescript-eslint'],
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/']
+      },
+      typescript: { alwaysTryTypes: true }
+    }
+  },
+  ignorePatterns: ['react-app-env.d.ts'],
+  rules: {
+    semi: ['error', 'never'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    'react/jsx-uses-vars': 'error',
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'error',
+    'react-hooks/exhaustive-deps': 'off',
+    'import/no-default-export': 'error',
+    '@typescript-eslint/return-await': ['error', 'in-try-catch'],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/promise-function-async': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: { caseInsensitive: true, order: 'asc' },
+        groups: ['builtin', 'external', 'internal', 'index', 'parent', 'sibling', 'object', 'type']
+      }
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'block' },
+      { blankLine: 'always', prev: 'block', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'block-like' },
+      { blankLine: 'always', prev: 'block-like', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] }
+    ],
+    'prettier/prettier': ['warn', require('./.prettierrc.js')]
+  },
+  settings: {
+    jest: {
+      version: 29
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "prettier"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        },
-    },
-    "plugins": [
-        "react",
-        "react-hooks",
-        "@typescript-eslint",
-        "prettier"
-    ],
-    "rules": {
-        "@typescript-eslint/no-unused-vars": "error",
-        "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-        "react/react-in-jsx-scope": "off",
-        "camelcase": "error",
-        "no-duplicate-imports": "error",
-        "@typescript-eslint/semi": ["error"],
-    },
-    "ignorePatterns": [
-        "dist",
-        ".eslintrc.js",
-        "postcss.config.js"
-    ],
+    react: {
+      version: '18.2.0'
+    }
+  },
+  "ignorePatterns": [
+    "dist",
+    ".eslintrc.js",
+    "postcss.config.js",
+    "src/**/*.test.js",
+    "src/**/*.test.ts",
+    "vite-env.d.ts",
+    "vite-config.ts"
+],
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+export const App = () => {
   return (
-      <div className="App">
-          <header className="App-header">
-              <img
-                  alt="logo"
-                  className="App-logo"
-                  src={logo}
-              />
-
-              <p>
-                  Edit 
-                  {' '}
-
-                  <code>
-                      src/App.ts
-                  </code>
-
-                  {' '}
-                  and save to reload.
-              </p>
-
-              <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  rel="noopener noreferrer"
-                  target="_blank"
-              >
-                  Learn React
-              </a>
-          </header>
-      </div>
-  );
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.ts</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
-
-export default App;
