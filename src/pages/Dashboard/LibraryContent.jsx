721cde37f54a326466aa201e4d57349e3d3fc4e4
mzulqarnain118 <muhammad.zulqarnain@devcrew.io>
2023-12-13 18:59:52 +0500

    Done changes in library for opening url on onclick

--- src/pages/Dashboard/LibraryContent.jsx
+++ src/pages/Dashboard/LibraryContent.jsx
@@ -4,59 +4,73 @@ import libraryStyles from "styles/components/libraryStyles";
 import common from "components/common";
 import defaultThumbnail from "assets/images/dummy.png";
 
+  export const handleLibraryItemClick = (url,type) => {
+      if (type === "pdf") {
+        const newTab = window.open();
+        newTab.document.write(
+          '<iframe src="' +
+            url +
+            '" style="width:100%; height:100vh; border:none;"></iframe>'
+        );
+      } else {
+        window.open(url, "_blank");
+      }
+  };
 const LibraryContent = ({ libraryData, typeIcons }) => {
   const classes = libraryStyles();
 
   return (
     <>
       {libraryData?.map((library) => (
-                <a className="cursor" href={library?.url} target="_blank" rel="noopener noreferrer">
-          <Box key={library.id} className={classes.mainBox}>
-            <common.Img src={defaultThumbnail} className={classes.contentImg} />
-            <Grid container>
-              <Grid item xs={10} sm={10} md={10}>
-                <Box className="col-start gap-025">
-                  <Typography sx={{ fontWeight: 600 }} align="left">
-                    {library.title}
-                  </Typography>
-                  <Typography
-                    variant="lightSubtitle1"
-                    align="left"
-                    dangerouslySetInnerHTML={{ __html: library.description }}
+        <Box
+          key={library.id}
+          className={`${classes.mainBox} cursor`}
+          onClick={() => handleLibraryItemClick(library.url, library.type)}
+        >
+          <common.Img src={defaultThumbnail} className={classes.contentImg} />
+          <Grid container>
+            <Grid item xs={10} sm={10} md={10}>
+              <Box className="col-start gap-025">
+                <Typography sx={{ fontWeight: 600 }} align="left">
+                  {library.title}
+                </Typography>
+                <Typography
+                  variant="lightSubtitle1"
+                  align="left"
+                  dangerouslySetInnerHTML={{ __html: library.description }}
+                />
+                <Box className="flex">
+                  <Avatar
+                    src={
+                      library?.created_by?.profile?.profile_pic ??
+                      defaultThumbnail
+                    }
+                    alt="Media"
+                    className={classes.contentAvatar}
                   />
-                  <Box className="flex">
-                    <Avatar
-                      src={
-                        library?.created_by?.profile?.profile_pic ??
-                        defaultThumbnail
-                      }
-                      alt="Media"
-                      className={classes.contentAvatar}
-                    />
-                    <Typography variant="subtitle2">
-                      {`${library.created_by.first_name} ${library.created_by.last_name}`}
-                    </Typography>
-                  </Box>
-
-                  <Box className={classes.flexStart}>
-                    {library.tags.map((tag) => (
-                      <Chip
-                        key={tag.title}
-                        label={tag.title}
-                        className={classes.contentChip}
-                      />
-                    ))}
-                  </Box>
+                  <Typography variant="subtitle2">
+                    {`${library.created_by.first_name} ${library.created_by.last_name}`}
+                  </Typography>
                 </Box>
-              </Grid>
 
-              <Grid item xs={2} className="col-between">
-                <Typography variant="lightSubtitle2">{library.date}</Typography>
-                  <common.Img type="icon" src={typeIcons[library?.type]} />
-              </Grid>
+                <Box className={classes.flexStart}>
+                  {library.tags.map((tag) => (
+                    <Chip
+                      key={tag.title}
+                      label={tag.title}
+                      className={classes.contentChip}
+                    />
+                  ))}
+                </Box>
+              </Box>
             </Grid>
-          </Box>
-          </a>
+
+            <Grid item xs={2} className="col-between">
+              <Typography variant="lightSubtitle2">{library.date}</Typography>
+              <common.Img src={typeIcons[library?.type]} />
+            </Grid>
+          </Grid>
+        </Box>
       ))}
     </>
   );
--- src/pages/Dashboard/ModuleView.jsx
+++ src/pages/Dashboard/ModuleView.jsx
@@ -3,6 +3,7 @@ import { Avatar, Box, Chip, Grid, Typography } from "@mui/material";
 import libraryStyles from "styles/components/libraryStyles";
 import common from "components/common";
 import defaultThumbnail from "assets/images/dummy.png";
+import { handleLibraryItemClick } from "./LibraryContent";
 
 const ModuleView = ({ libraryData, typeIcons }) => {
   const classes = libraryStyles();
@@ -10,72 +11,67 @@ const ModuleView = ({ libraryData, typeIcons }) => {
   return (
     <Grid container spacing={2} padding={"0.75rem 1.5rem"}>
       {libraryData?.map((library) => (
-        <a
+        <Grid
+          key={library.id}
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          lg={4}
+          mt={"1.25rem"}
           className="cursor"
-          href={library?.url}
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={() => handleLibraryItemClick(library.url, library.type)}
         >
-          <Grid
-            key={library.id}
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            lg={4}
-            mt={"1.25rem"}
-          >
-            <Box>
-              <div className="relative-full-width">
-                <common.Img
-                  src={defaultThumbnail}
-                  className={classes.moduleContentImg}
-                />
-                <div className={classes.moduleContentSource}>
-                  {typeIcons[library.type] == "youtube" && (
-                    <common.Img type="icon" src={typeIcons[library.type]} />
-                  )}
-                </div>
+          <Box>
+            <div className="relative-full-width">
+              <common.Img
+                src={defaultThumbnail}
+                className={classes.moduleContentImg}
+              />
+              <div className={classes.moduleContentSource}>
+                {typeIcons[library.type] == "youtube" && (
+                  <common.Img type="icon" src={typeIcons[library.type]} />
+                )}
               </div>
+            </div>
 
-              <Box className="flex" mt={"12px"}>
-                <Grid container spacing={0.5}>
-                  <Grid item xs={11} sm={11} md={11} lg={11}>
-                    <Typography sx={{ fontWeight: 600 }} align="left">
-                      {library.title}
-                    </Typography>
-                    <Box
-                      className="flex"
-                      sx={{
-                        alignItems: "flex-start",
-                        gap: "0.625rem",
-                        maxHeight: "80px", // Adjust the height accordingly
-                        overflowY: "auto", // Make it scrollable
-                      }}
-                    >
-                      {library.tags.map((tag, index) => (
-                        <Chip
-                          key={tag.title}
-                          label={tag.title}
-                          className={classes.moduleContentChip}
-                        />
-                      ))}
-                    </Box>
-                  </Grid>
-                  <Grid item xs={1}>
-                    <Avatar
-                      src={
-                        library?.created_by?.profile?.profile_pic ??
-                        defaultThumbnail
-                      }
-                      className={classes.moduleContentAvatar}
-                    />
-                  </Grid>
+            <Box className="flex" mt={"12px"}>
+              <Grid container spacing={0.5}>
+                <Grid item xs={11} sm={11} md={11} lg={11}>
+                  <Typography sx={{ fontWeight: 600 }} align="left">
+                    {library.title}
+                  </Typography>
+                  <Box
+                    className="flex"
+                    sx={{
+                      alignItems: "flex-start",
+                      gap: "0.625rem",
+                      maxHeight: "80px", // Adjust the height accordingly
+                      overflowY: "auto", // Make it scrollable
+                    }}
+                  >
+                    {library.tags.map((tag, index) => (
+                      <Chip
+                        key={tag.title}
+                        label={tag.title}
+                        className={classes.moduleContentChip}
+                      />
+                    ))}
+                  </Box>
                 </Grid>
-              </Box>
+                <Grid item xs={1}>
+                  <Avatar
+                    src={
+                      library?.created_by?.profile?.profile_pic ??
+                      defaultThumbnail
+                    }
+                    className={classes.moduleContentAvatar}
+                  />
+                </Grid>
+              </Grid>
             </Box>
-          </Grid>
-        </a>
+          </Box>
+        </Grid>
       ))}
     </Grid>
   );
