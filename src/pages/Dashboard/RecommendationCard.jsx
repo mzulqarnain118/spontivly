8e889ecac2f7fd061fa8c389901d37c2bda17502
mzulqarnain118 <muhammad.zulqarnain@devcrew.io>
2024-01-04 19:11:03 +0500

    started working on recommendations

--- src/pages/Dashboard/RecommendationCard.jsx
+++ src/pages/Dashboard/RecommendationCard.jsx
@@ -1,70 +1,124 @@
-import { Avatar, Box, Card, CardContent, Grid, Typography } from '@mui/material'
+import { Avatar, Box, Card, CardContent, Grid, Typography, Divider } from '@mui/material'
 import React, { Fragment } from 'react'
+import { useInfiniteQuery } from 'react-query'
 import { useSelector } from 'react-redux'
+import { ApiCall, encodeParams } from 'utils'
 import { Controls as common } from '../../components/common'
 import { channelStyles } from '../Channels/channelStyles'
 import { Events } from './Events'
-const data = [
-  {
-    name: 'Scott Lang',
-    action: 'are both interested in',
-    purposes: ['Climate Change', 'Quantum Computing', 'Quantum Computing']
-  },
-  {
-    name: 'James Rhodes',
-    action: 'share the objective of',
-    purposes: ['Hiring / Recruiting']
-  },
-  {
-    name: 'Natasha Romanoff',
-    action: 'are both interested in',
-    purposes: ['Defense Technology']
-  }
-]
 
 function RecommendationCard() {
   const currentUser = useSelector((state) => state?.dashboard?.currentUser)
   const isFavorite = (id) => currentUser?.favorites?.some((item) => item.id == id)
   const classes = channelStyles()
 
+  async function fetchRecommendations({ pageParam = 1 }) {
+    const queryParams = {
+      page: pageParam,
+      sort: 'Recommendation'
+    }
+    const encodedParams = encodeParams(queryParams)
+    const apiUrl = `profile?${encodedParams}`
+
+    return ApiCall(apiUrl)
+  }
+
+  const {
+    data: recommendations,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status
+  } = useInfiniteQuery(
+    ['profile'], // Dynamic query key
+    ({ pageParam = 1 }) => fetchRecommendations({ pageParam }),
+    {
+      getNextPageParam: (lastPage) => lastPage?.next
+    }
+  )
+
   return (
     <Grid container spacing={2}>
-      <Events />
-      <Grid item xs={12}>
+      <Grid container item>
+        <Events />
+      </Grid>
+
+      <Grid container item>
         <Card className={classes.container}>
           <CardContent className="col-start gap-1">
             <Typography variant="h6">Recommendations</Typography>
-            {data.map((recommendation) => (
-              <Fragment key={recommendation?.name}>
-                <Box className="row gap-1">
-                  <Avatar src={recommendation?.profile_pic} />
-                  <Box className="recommendation-details col-start gap-05">
-                    <Box className="row-start gap-05">
-                      <Typography variant="author">{recommendation?.user?.first_name + recommendation?.user?.last_name}</Typography>
-                      {isFavorite(recommendation?.id) ? (
-                        <common.MuiIcon name="StarRateRounded" color="warning.main" onClick={() => addFavorites(recommendation?.id)} />
-                      ) : (
-                        <common.MuiIcon name="StarBorderRounded" color="primary.lighter" onClick={() => addFavorites(recommendation?.id)} />
+            <common.InfiniteQueryWrapper
+              status={status}
+              data={recommendations}
+              error={error}
+              fetchNextPage={fetchNextPage}
+              hasNextPage={hasNextPage}
+              isFetchingNextPage={isFetchingNextPage}
+              isFetching={isFetching}
+            >
+              {(recommendations) =>
+                recommendations.map((recommendation, index) => (
+                  <Grid container item direction="column" key={recommendation?.id} wrap="nowrap">
+                    <Grid item xs={12} className="row gap-1">
+                      <Avatar src={recommendation?.profile_pic} />
+                      <Box className="recommendation-details col-start gap-05">
+                        <Box className="row-start gap-05">
+                          <Typography variant="author">{recommendation?.user?.first_name + recommendation?.user?.last_name}</Typography>
+                          {isFavorite(recommendation?.id) ? (
+                            <common.MuiIcon name="StarRateRounded" color="warning.main" onClick={() => addFavorites(recommendation?.id)} />
+                          ) : (
+                            <common.MuiIcon
+                              name="StarBorderRounded"
+                              color="primary.lighter"
+                              onClick={() => addFavorites(recommendation?.id)}
+                            />
+                          )}
+                        </Box>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={12}>
+                      {recommendation?.objectives?.length !== 0 && (
+                        <Typography variant="lightSubtitle2">
+                          You and {recommendation?.user?.first_name ?? ''} share the objective of {' '}
+                          <Typography sx={{ color: 'primary.main', fontSize: '12px', fontWeight: '600' }} component="span">
+                            {recommendation?.objectives?.map((purpose, index) => (
+                              <span key={purpose?.id}>
+                                {index > 0 && index === recommendation?.objectives?.length - 1 ? (
+                                  <span style={{ color: 'primary.light' }}> and </span>
+                                ) : null}
+                                {index > 0 && index < recommendation?.objectives?.length - 1 ? ', ' : ''}
+                                {purpose?.title ?? ''}
+                              </span>
+                            ))}
+                          </Typography>
+                        </Typography>
                       )}
-                    </Box>
-                  </Box>
-                </Box>
-                <Typography align="start" variant="lightSubtitle2">
-                  You and {recommendation?.name ?? ''} {recommendation?.action ?? ''}{' '}
-                  <Typography sx={{ color: 'primary.main', fontSize: '12px', fontWeight: '600' }} component="span">
-                    {recommendation?.purposes.map((purpose, index) => (
-                      <span key={index}>
-                        {index > 0 && index === recommendation?.purposes?.length - 1 ? (
-                          <span style={{ color: 'primary.light' }}> and </span>
-                        ) : null}
-                        {index > 0 && index < recommendation?.purposes.length - 1 ? ', ' : ''}
-                        {purpose ?? ''}
-                      </span>
-                    ))}
-                  </Typography>
-                </Typography>
-              </Fragment>
-            ))}
+                    </Grid>
+                    <Grid item xs={12}>
+                      {recommendation?.interests?.length !== 0 && (
+                        <Typography variant="lightSubtitle2">
+                          You and {recommendation?.user?.first_name ?? ''} are both interested in{' '}
+                          <Typography sx={{ color: 'primary.main', fontSize: '12px', fontWeight: '600' }} component="span">
+                            {recommendation?.interests?.map((purpose, index) => (
+                              <span key={purpose?.id}>
+                                {index > 0 && index === recommendation?.interests?.length - 1 ? (
+                                  <span style={{ color: 'primary.light' }}> and </span>
+                                ) : null}
+                                {index > 0 && index < recommendation?.interests?.length - 1 ? ', ' : ''}
+                                {purpose?.title ?? ''}
+                              </span>
+                            ))}
+                          </Typography>
+                        </Typography>
+                      )}
+                    </Grid>
+                    <div className={` ${index < recommendations.length - 1 ? 'divider' : ''}`}></div>
+                  </Grid>
+                ))
+              }
+            </common.InfiniteQueryWrapper>
           </CardContent>
         </Card>
       </Grid>
