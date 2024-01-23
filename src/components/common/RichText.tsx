9c5e7d36d478e01527e4fef89dae775a606c9bd0
mzulqarnain118 <muhammad.zulqarnain@devcrew.io>
2024-01-23 18:45:47 +0500

    fixed issue in build

--- src/components/common/RichText.tsx
+++ src/components/common/RichText.tsx
@@ -1,65 +1,25 @@
-import JoditEditor from 'jodit-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import { CKEditor } from '@ckeditor/ckeditor5-react'
 import React, { useRef } from 'react'
 import { commonStyles } from '../../styles/commonStyles'
 
-export function RichText({ value, onBlur, className, placeholder, ...others }: any) {
-  const editor = useRef(null)
-  const classes: any = commonStyles()
-  const config = {
-    readonly: false,
-    height: 400,
-    className: className ?? classes.editor,
-    placeholder: placeholder || 'Description',
-    ...others,
-    // toolbarAdaptive: false,
-    buttons: [
-      'source',
-      '|',
-      'bold',
-      'italic',
-      'underline',
-      'strikethrough',
-      '|',
-      'ul',
-      'ol',
-      '|',
-      'indent',
-      'outdent',
-      '|',
-      'font',
-      'fontsize',
-      '|',
-      'table',
-      '|',
-      'align',
-      'undo',
-      'redo',
-      '|',
-      'selectall',
-      'fullsize',
-      '|',
-      'copyformat',
-      'hr',
-      'symbol',
-      'fullsize',
-      'print',
-      'about'
-    ]
-  }
+export function RichText({ data, onReady, onChange, onFocus, value, onBlur, className, required, placeholder, ...others }: any) {
+  const editorRef = useRef()
 
   return (
-    <>
-      <JoditEditor ref={editor} value={value} config={config} onBlur={(newContent) => onBlur && onBlur(newContent)} />
-    </>
+    <CKEditor
+      editor={ClassicEditor}
+      data={data}
+      onReady={(editor) => {
+        editorRef.current = editor
+        if (onReady) onReady(editor)
+      }}
+      onChange={onChange}
+      onBlur={(newContent) => onBlur && onBlur(newContent)}
+      onFocus={onFocus}
+      required // Add the required attribute
+    />
   )
 }
 //!  FOR MORE INFO ABOUT THIS COMPONENT VISIT THIS LINK https://www.npmjs.com/package/jodit-react or
 //! use CSK - EDITOR https://ckeditor.com/docs/ckeditor5/latest/installation/integrations/react.html
-//TODO: CALL THIS COMPONENT IN THE PARENT COMPONENT
-//   const handleBodyChange = (newContent) => {
-//     setValues((old) => ({ ...old, ["request_body"]: newContent }));
-//   };
-//                     <RichText
-//                       content={values.request_body}
-//                       setContent={handleBodyChange}
-//                     />;
